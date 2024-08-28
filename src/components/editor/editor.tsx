7152c208e5f1dafb5dd7982aaf12e13ef8a56e77
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-08-28 19:29:45 +0100

    feat: Listen to text change event

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,25 +1,40 @@
 import Quill from "quill";
-import { forwardRef, useEffect, useRef, useState } from "react";
-import "./editor.css"
+import { FormEvent, forwardRef, useEffect, useRef, useState } from "react";
+import "./editor.css";
+import "quill/dist/quill.core.css";
 
 function Editor() {
   const [state, setState] = useState();
+  const toolbar = useRef<HTMLDivElement | null>(null);
   const editorRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(
     () => {
       if (editorRef.current) {
         const quill = new Quill(editorRef.current, {
-          modules: { toolbar: true }
+          theme: "snow"
         });
-        quill.insertText(0, "Inserted first text\n\n")
+        quill.insertText(0, "Inserted first text\n\n");
+        // console.log("content => ", quill.getContents())
         // editorRef.current = quill;
+        quill.on(Quill.events.TEXT_CHANGE, () => {
+          console.log("contents => ", quill.getContents());
+        })
       }
     },
     [editorRef.current]
   )
 
+  function handleTyping(event: FormEvent) {
+    console.log("event fired?");
+  }
+
   return(
-    <div id="editor" ref={editorRef}></div>
+    <>
+      <div id="toolbar" ref={toolbar}></div>
+      <div id="editor" ref={editorRef} onChange={handleTyping}></div>
+      <button onClick={handleTyping}>Get Contents</button>
+    </>
   )
 }
 
